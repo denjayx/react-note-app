435a9b24ab0a51b4901df718dc3be6d5a4b1d475
denjayx <denyw602@gmail.com>
2024-01-01 02:00:45 +0700

    starter

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// import style
+import './styles/style.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<h1>Hello, World!</h1>);
