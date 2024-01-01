39c4570a64e22e3b8fc07c7de710752d6d7be489
denjayx <denyw602@gmail.com>
2024-01-01 14:57:54 +0700

    note archived

---
+++ src/components/NoteArchived.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NoteItem from "./NoteItem";
+
+const NoteArchived = ({notes, showFormattedDate, onDelete, onArchive}) => {
+	return (
+		<div>
+			<h2>Catatan Di Arsipkan</h2>
+			<div className="notes-list">{notes.filter((note) => note.archived).length === 0 ? <p>Tidak ada catatan diarsipkan</p> : notes.filter((note) => note.archived).map((note) => <NoteItem className="notes-item" key={note.id} {...note} showFormattedDate={showFormattedDate} onDelete={onDelete} onArchive={onArchive} />)}</div>
+		</div>
+	);
+};
+
+export default NoteArchived;
