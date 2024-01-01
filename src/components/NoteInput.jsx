ca9485ce99e3d434ed0f24bb2adb30d059037cfb
denjayx <denyw602@gmail.com>
2024-01-01 20:22:57 +0700

    char limit

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 
 export const NoteInput = ({addNote}) => {
-	const [title, setTitle] = useState();
-	const [body, setBody] = useState();
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
+	const maxTitleLength = 20;
+	const maxBodyLength = 50;
 
 	const onSubmitHandler = (e) => {
 		e.preventDefault();
@@ -20,13 +22,29 @@ export const NoteInput = ({addNote}) => {
 			}
 		}
 	};
+
+	const handleTitleChange = (e) => {
+		const inputValue = e.target.value;
+		if (inputValue.length <= maxTitleLength) {
+			setTitle(inputValue);
+		}
+	};
+
+	const handleBodyChange = (e) => {
+		const inputValue = e.target.value;
+		if (inputValue.length <= maxBodyLength) {
+			setBody(inputValue);
+		}
+	};
+
 	return (
 		<div className="note-input">
 			<h2>Buat Catatan</h2>
 			<form onSubmit={onSubmitHandler}>
-				<p className="note-input__title__char-limit">Sisa Karakter : 50</p>
-				<input type="text" className="note-input__title" placeholder="Judul Catatan" value={title} onChange={(e) => setTitle(e.target.value)} />
-				<textarea name="input-body" placeholder="Tuliskan Catatan Disini" cols="30" rows="10" className="note-input__body" value={body} onChange={(e) => setBody(e.target.value)} />
+				<p className="note-input__title__char-limit">Sisa Karakter Judul: {maxTitleLength - title.length}</p>
+				<input type="text" className="note-input__title" placeholder="Judul Catatan" value={title} onChange={handleTitleChange} />
+				<p className="note-input__title__char-limit">Sisa Karakter Catatan: {maxBodyLength - body.length}</p>
+				<textarea name="input-body" placeholder="Tuliskan Catatan Disini" cols="30" rows="10" className="note-input__body" value={body} onChange={handleBodyChange} />
 				<button type="submit">Buat</button>
 			</form>
 		</div>
